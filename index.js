99295e53d443666bc1913a9f76a8cc5883eb2ccb
david robison <mrdavidrobison@gmail.com>
2017-05-24 11:29:12 -0700

    change twitter output

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ $(document).ready(function () {
 
   function getQuote() {
     var quotes = ["There is a way that appears to be right, but in the end it leads to death.", "Where there is no revelation, people cast off restraint; but blessed are those who heed wisdom's instruction.", "For receiving instruction in prudent behavior, doing what is right and just and fair", "A cheerful heart is good medicine, but a crushed spirit dries up the bones.", "Pride goes before destruction, a haughty spirit before a fall.", "A gentle answer turns away wrath, but a harsh word stirs up anger."];
-    var author = ["King Soloman"];
+    var author = ["-King Soloman","-King Soloman","-King Soloman","-King Soloman","-King Soloman","-King Soloman"];
 
     var randomNum = Math.floor(Math.random() * quotes.length);
     randomQuote = quotes[randomNum];
@@ -15,7 +15,7 @@ $(document).ready(function () {
   }
 
   $("#tweet").on("click", function () {
-    window.open("https://twitter.com/intent/tweet?text=" + randomQuote);
+    window.open("https://twitter.com/intent/tweet?text=" + randomQuote + randomAuthor);
   });
   $("#newQuote").on("click", function () {
     getQuote();
