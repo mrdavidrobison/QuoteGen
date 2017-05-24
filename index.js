c9387fb907d9569f7b0a3f2a421593523b094bf0
david robison <mrdavidrobison@gmail.com>
2017-05-24 11:16:25 -0700

    updated quotes

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
-$(document).ready(function() {
+$(document).ready(function () {
   var randomQuote;
   var author;
+
   function getQuote() {
-    var quotes = ["Hello, John", "Hello, Sea", "Hello Land", "Hello Sky"];
-    var author = ["author1", "author2", "author3", "author4"];
+    var quotes = ["There is a way that appears to be right, but in the end it leads to death.", "Where there is no revelation, people cast off restraint; but blessed are those who heed wisdom's instruction.", "For receiving instruction in prudent behavior, doing what is right and just and fair", "A cheerful heart is good medicine, but a crushed spirit dries up the bones.", "Pride goes before destruction, a haughty spirit before a fall.", "A gentle answer turns away wrath, but a harsh word stirs up anger."];
+    var author = ["King Soloman"];
 
     var randomNum = Math.floor(Math.random() * quotes.length);
     randomQuote = quotes[randomNum];
@@ -13,10 +14,10 @@ $(document).ready(function() {
     $(".author").text(randomAuthor);
   }
 
-  $("#tweet").on("click", function() {
+  $("#tweet").on("click", function () {
     window.open("https://twitter.com/intent/tweet?text=" + randomQuote);
   });
-  $("#newQuote").on("click", function() {
+  $("#newQuote").on("click", function () {
     getQuote();
   });
 });
